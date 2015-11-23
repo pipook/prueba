7df4cfc2958632cf2b55006f918663d96164730c
Daniel Chico <pipook@gmail.com>
2015-11-22 22:15:14 -0500

    Comentario prueba

---
+++ pruebaGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pruebaGit
+//
+//  Created by Daniel Enrique Chico Pájaro on 22/11/15.
+//  Copyright © 2015 Daniel Enrique Chico Pájaro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
